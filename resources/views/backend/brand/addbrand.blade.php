8ecf8ad28ebc7a70521ee5ca29ca7ce66a360942
saradiberardino9712 <sara.9712@hotmail.it>
2019-05-18 16:46:42 +0200

    modifiche varie grafiche backend

--- resources/views/backend/brand/addbrand.blade.php
+++ resources/views/backend/brand/addbrand.blade.php
@@ -1,13 +1,32 @@
 @extends('backend.layout')
 
 @section('content')
+
+    <div class="breadcrumbs">
+        <div class="col-sm-4">
+            <div class="page-header float-left">
+                <div class="page-title">
+                    <h1>Aggiungi Brand</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="page-header float-right">
+                <div class="page-title">
+                    <ol class="breadcrumb text-right">
+                        <li><a href="{{route('admin.index')}}">Home</a></li>
+                        <li>Gestione Brand</li>
+                        <li class="active">Aggiungi Brand</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <form action="{{route('admin.addBrandCreate')}}" method="post" class="form-horizontal"> <!-- enctype="multipart/form-data" -->
     @csrf
     <!--<div class="col-lg-6"> eliminato per togliere style che andavano in contrasto con i margini inseriti a riga 23-->
     <div class="card add"> <!-- aggiunta class "add" per mettere dei margini al form-->
-        <div class="card-header">
-            <strong>Aggiungi Brand</strong>
-        </div>
         <div>
         <div class="card-body card-block">
             <div class="row form-group">
@@ -17,7 +36,7 @@
         </div>
         <div class="card-footer">
             <button type="submit" class="btn btn-primary btn-sm">
-                <i class="fa fa-dot-circle-o"></i> Submit
+                <i class="fa fa-dot-circle-o"></i> Aggiungi
             </button>
             <button type="reset" class="btn btn-danger btn-sm">
                 <i class="fa fa-ban"></i> Reset
--- resources/views/backend/brand/editbrand.blade.php
+++ resources/views/backend/brand/editbrand.blade.php
@@ -1,13 +1,32 @@
 @extends('backend.layout')
 
 @section('content')
+
+    <div class="breadcrumbs">
+        <div class="col-sm-4">
+            <div class="page-header float-left">
+                <div class="page-title">
+                    <h1>Modifica Brand</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="page-header float-right">
+                <div class="page-title">
+                    <ol class="breadcrumb text-right">
+                        <li><a href="{{route('admin.index')}}">Home</a></li>
+                        <li>Gestione Brand</li>
+                        <li class="active">Modifica Brand</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <form action="{{route('admin.editBrandUpdate')}}" method="post" class="form-horizontal">
     @csrf
         <!--<div class="col-lg-6"> eliminato per togliere style che andavano in contrasto con i margini inseriti a riga 23-->
         <div class="card add"> <!-- aggiunta class "add" per mettere dei margini al form-->
-            <div class="card-header">
-                <strong>Modifica Brand</strong>
-            </div>
             <div class="card-body card-block">
                 <div class="row form-group">
                     <div class="col col-md-3"><label for="brand" class=" form-control-label">Brand</label></div>
@@ -23,14 +42,14 @@
                 </div>
                 <div class="row form-group">
                     <div class="col col-md-3"><label for="text-input" class=" form-control-label">Nome Brand</label></div>
-                    <div class="col-12 col-md-9"><input value="" type="text" id="text-input newname" name="newname" placeholder="Selezionare il Brand da modificare" class="form-control">
+                    <div class="col-12 col-md-9"><input value="" type="text" id="text-input newname" name="newname" placeholder="Inserisci il nuovo nome del brand" class="form-control">
                         <small class="help-block form-text">Inserisci il nuovo nome da assegnare al brand</small>
                     </div>
                 </div>
             </div>
             <div class="card-footer">
                 <button type="submit" class="btn btn-primary btn-sm">
-                    <i class="fa fa-dot-circle-o"></i> Submit
+                    <i class="fa fa-dot-circle-o"></i> Modifica
                 </button>
                 <button type="reset" class="btn btn-danger btn-sm">
                     <i class="fa fa-ban"></i> Reset
