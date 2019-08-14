f341f66d7b3530344d153294eb25adffeba4d9c9
AposVinc <apos.vincenzo@gmail.com>
2019-08-14 18:51:03 +0200

    filter shop.blade

---
+++ resources/views/frontend/partials/filter.blade.php
@@ -0,0 +1,91 @@
+<div id="column-left" class="col-sm-4 col-lg-3 ">
+    <div class="filter left-sidebar-widget mb_50">
+        <div class="heading-part mtb_20 ">
+            <h2 class="main_title">Applica i Filtri</h2>
+        </div>
+        <div class="filter-block">
+            <p>
+                <label for="amount">Range Prezzo:</label>
+                <input type="text" id="amount" readonly>
+            </p>
+            <div id="slider-range" class="mtb_20"></div>
+            <div class="list-group">
+                <div class="list-group-item mb_10">
+                    <label>Genere</label>
+                    <div id="filter-group1">
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Uomo </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Donna </label>
+                        </div>
+                        <div class="checkbox ">
+                            <label>
+                                <input value="" type="checkbox"> Unisex
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="list-group-item mb_10">
+                    <label>Brand</label>
+                    <div id="filter-group2">
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Breil </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Casio </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Wellington </label>
+                        </div>
+                        <div class="checkbox ">
+                            <label>
+                                <input value="" type="checkbox"> Swatch
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="list-group-item mb_10">
+                    <label>Colore</label>
+                    <div id="filter-group3">
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Blu </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Nero </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Bianco </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="list-group-item mb_10">
+                    <label>Materiale</label>
+                    <div id="filter-group4">
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Acciaio </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Legno </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input value="" type="checkbox"> Pelle </label>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn">Filtra</button>
+            </div>
+        </div>
+    </div>
+</div>
