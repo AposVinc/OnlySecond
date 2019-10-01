46155a971cf039342aa976b0852a2112042ac055
AposVinc <apos.vincenzo@gmail.com>
2019-10-01 18:38:04 +0200

    Delete Miei indirizzi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,10 +33,10 @@ class UserController extends Controller
     {
         $addresses = Auth::user()->addresses()->get();
 
-        $old_mailing = $addresses->where('mailing','=','1')->first();
-        $old_billing = $addresses->where('billing','=','1')->first();
-        $new_mailing = $addresses->where('id','=',$request->mailing_address)->first();
-        $new_billing = $addresses->where('id','=',$request->billing_address)->first();
+        $old_mailing = $addresses->where('mailing','1')->first();
+        $old_billing = $addresses->where('billing','1')->first();
+        $new_mailing = $addresses->where('id',$request->mailing_address)->first();
+        $new_billing = $addresses->where('id',$request->billing_address)->first();
 
         if ($request->has('mailing_address')){
             $old_mailing->mailing = 0;
@@ -77,25 +77,37 @@ class UserController extends Controller
     public function deleteAddress(Request $request)
     {
         $addresses = Auth::user()->addresses()->get();
+        if ($addresses->count() > 1){
+            $old_mailing = $addresses->where('mailing','1')->first();
+            $old_billing = $addresses->where('billing','1')->first();
+            $delete_address = $addresses->where('id',$request->delete_address)->first();
+            if ($delete_address == $old_mailing){
+                $new_m_address = $addresses->where('id','<>',$request->delete_address)->first();
+                $new_m_address->mailing = 1;
+            }
+            if ($delete_address == $old_billing){
+                $new_b_address = $addresses->where('id','<>',$request->delete_address)->first();
+                $new_b_address->billing = 1;
+            }
 
-        $old_mailing = $addresses->where('mailing','=','1')->first();
-        $old_billing = $addresses->where('billing','=','1')->first();
-        $deleted_address = $addresses->where('billing','=',$request->delete_address)->first();
-        $new_mb_address = $addresses->first();
-
-        if ($request->has('mailing_address')){
-            $old_mailing->mailing = 0;
-            $new_mb_address->mailing = 1;
+            if (isset($new_m_address)) {
+                if (!($new_m_address->save())){
+                    return back()->with('error','Errore durante il caricamento. Riprovare!!');
+                }
+            }
+            if (isset($new_b_address)) {
+                if (!($new_b_address->save())){
+                    return back()->with('error','Errore durante il caricamento. Riprovare!!');
+                }
+            }
+            if ($delete_address->delete()){
+                return back()->with('success', 'Caricamento avvenuto con successo!!');
+            } else{
+                return back()->with('error','Errore durante il caricamento. Riprovare!!');
+            }
+        }else{
+            return back()->with('error','Prima di eliminare questo indirizzo aggiungine un altro');
         }
-        if ($request->has('billing_address')){
-            $old_billing->billing = 0;
-            $new_mb_address->billing = 1;
-        }
-
-        if ($old_mailing->save() and $old_billing->save() and $new_mb_address->save()){
-            return back()->with('success', 'Caricamento avvenuto con successo!!');
-        }
-        return back()->with('error','Errore durante il caricamento. Riprovare!!');
     }
 
 }
