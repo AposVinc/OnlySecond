46a30b849cb8447665bad904c5fb99f4f8b05730
ValeeeR <37934589+ValeeeR@users.noreply.github.com>
2019-10-02 10:57:59 +0200

    Update slideroffers.blade.php

--- resources/views/frontend/partials/slideroffers.blade.php
+++ resources/views/frontend/partials/slideroffers.blade.php
@@ -6,7 +6,7 @@
         <div class="col-sm-12 mtb_10">
             <div class="mt_60">
                 <div class="heading-part mb_10 ">
-                    <h2 class="main_title">Deals of the Week</h2>
+                    <h2 class="main_title">Offerte Della Settimana</h2>
                 </div>
                 <div class="latest_pro box">
                     <div class="latest owl-carousel">
@@ -17,7 +17,7 @@
                                     <div class="product-thumb">
                                         <div class="image product-imageblock"> <a href="{{route('Product', ['cod' => $offer->product->cod])}}"> <img data-name="product_image" src="{{asset($offer->product->images->where('main',1)->first()->path_image)}}" alt="iPod Classic" title="iPod Classic" class="img-responsive"> <img src="{{asset($offer->product->images->where('main',1)->first()->path_image)}}" alt="iPod Classic" title="iPod Classic" class="img-responsive"> </a>
                                             <div class="button-group text-center">
-                                                <a href="{{route('Wishlist.AddProduct', ['cod' => $product->cod])}}"  class="wishlist"><span>wishlist</span></a>
+                                                <a href="{{route('Wishlist.AddProduct', ['cod' => $offer->cod])}}"  class="wishlist"><span>wishlist</span></a>
                                                 <a href="#" class="quickview"><span>Quick View</span></a>
                                                 <a href="#" class="compare"><span>Compare</span></a>
                                                 <a href="#" class="add-to-cart"><span>Add to cart</span></a>
