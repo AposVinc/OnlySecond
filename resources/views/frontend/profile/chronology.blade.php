04a662a35a3832d10495f489a2dbd43d38328c42
AposVinc <apos.vincenzo@gmail.com>
2019-10-05 16:15:17 +0200

    Grafica Storico ordini

--- resources/views/frontend/profile/chronology.blade.php
+++ resources/views/frontend/profile/chronology.blade.php
@@ -13,59 +13,70 @@
 
                 @foreach(auth()->user()->orderHistories()->get() as $order )
 
-                    <div class="heading-part mt_20 mb_10">
-                        <div class="col-md-2 mr-md-2">
-                            <label class="sub_title">N° Ordine: </label>
-                            <label class="text_title">{{$order->id}}</label>
+                    <div class="heading-part mb_10">
+                        <div class="col-md-12">
+                            <div class="pl_0 col-md-2 ">
+                                <label class="sub_title">N° Ordine: </label>
+                                <label class="text_title">{{$order->id}}</label>
+                            </div>
+                            <div class="pl_0 col-md-3">
+                                <div class="text_title" style="color: #fff;" >Data: </div>
+                                <div class="text_title">{{date('d-m-Y H:i', strtotime($order->created_at))}}</div>
+                            </div>
+                            <div class="col-md-offset-2 col-md-2">
+                                <label class="text_title" style="color: #fff;">Regalo: </label>
+                                <label class="text_title">@if($order->gift) Si @else No @endif</label>
+                            </div>
+                            <div class="pull-right">
+                                <label class="sub_title">Prezzo Totale: </label>
+                                <label class="text_title">{{$order->calculateTotalPrice()}} €</label>
+                            </div>
                         </div>
-                        <div class="sub_title" style="font-style: italic; color: #fff;" >Data: </div>
-                        <div class="text_title">{{date('d-m-Y H:i', strtotime($order->created_at))}}</div>
                     </div>
                     <div class="row">
-
-                        @foreach($order->products as $product)
-                            <div class="col-lg-12 mb_10">
-                                <div class="mt_10 col-md-2">
-                                    <div class="image product-imageblock ">
-                                        <a href="{{route('Product', ['cod' => $product->cod])}}">
-                                            <img data-name="product_image" src="{{asset($product->images->where('main',1)->first()->path_image)}}" alt="iPod Classic" title="iPod Classic" class="img-responsive">
-                                        </a>
-                                    </div>
+                        <div class="col-lg-12 mb_20">
+                            <div id="review" class="mt_10 mr_5 col-md-3 pull-right" style="text-align: right">
+                                <div>
+                                    <label><span>Indirizzo di Fatturazione:</span><br>{{$order->billingAddress->name}} {{$order->billingAddress->surname}} <br> {{$order->billingAddress->address}}, {{$order->billingAddress->civic_number}}<br>{{$order->billingAddress->city}} {{'('. $order->billingAddress->region. ')'}} CAP:{{$order->billingAddress->zip}}</label>
                                 </div>
-                                <div id="review" class="mt_10 col-md-10">
-                                    <div>
-                                        <label class="product-name"><span>Prodotto:</span>{{$product->collection->brand->name}} {{$product->collection->name}} - {{$product->cod}}</label>
-                                    </div>
-                                    <div>
-                                        <label><span>Prezzo:</span>{{$product->pivot->price}}</label>
-                                    </div>
-                                    <div>
-                                        <label><span>Quantità Acquistata:</span>{{$product->pivot->quantity}}</label>
-                                    </div>
+                                <div>
+                                    <label><span>Metodo di Pagamento:</span><br>{{$order->payment->name}}</label>
+                                </div>
+
+                                <div>
+                                    <label><span>Indirizzo di Spedizione:</span><br>{{$order->mailingAddress->name}} {{$order->mailingAddress->surname}} <br> {{$order->mailingAddress->address}}, {{$order->mailingAddress->civic_number}}<br>{{$order->mailingAddress->city}} {{'('. $order->mailingAddress->region. ')'}} CAP:{{$order->mailingAddress->zip}}</label>
+                                </div>
+                                <div>
+                                    <label><span>Corriere:</span>{{$order->courier->name}}</label>
                                 </div>
                             </div>
-                        @endforeach
+                            @foreach($order->products as $product)
+                                <div id="review" class="mb_10 col-md-8">
 
-                            qui sotto comuni a tutto l'ordine --- mettere visualizza altro?
-                            <div>
-                                <label><span>Metodo di Pagamento:</span>{{$order->payment->name}}</label>
-                            </div>
-                            <div>
-                                <label><span>Regalo:</span> @if($order->gift) Si @else No @endif
-                                </label>
-                            </div>
-                            <div>
-                                <label><span>Prezzo Totale:</span>{{$order->calculateTotalPrice()}}</label>
-                            </div>
-                            <div>
-                                <label><span>Indirizzo di Spedizione:</span>{{$order->mailingAddress->name}} {{$order->mailingAddress->surname}} - {{$order->mailingAddress->address}}, {{$order->mailingAddress->civic_number}} {{$order->mailingAddress->city}} {{'('. $order->mailingAddress->region. ')'}} CAP:{{$order->mailingAddress->zip}}</label>
-                            </div>
-                            <div>
-                                <label><span>Corriere:</span>{{$order->courier->name}}</label>
-                            </div>
-                            <div>
-                                <label><span>Indirizzo di Fatturazione:</span>{{$order->billingAddress->name}} {{$order->billingAddress->surname}} - {{$order->billingAddress->address}}, {{$order->billingAddress->civic_number}} {{$order->billingAddress->city}} {{'('. $order->billingAddress->region. ')'}} CAP:{{$order->billingAddress->zip}}</label>
-                            </div>
+                                    <div class="mt_10 pl_0 col-md-3">
+                                        <div class="image product-imageblock">
+                                            <a href="{{route('Product', ['cod' => $product->cod])}}">
+                                                <img data-name="product_image" src="{{asset($product->images->where('main',1)->first()->path_image)}}" alt="iPod Classic" title="iPod Classic" class="img-responsive" height="175" width="175">
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div id="review" class="mt_10 pl_0 col-md-8">
+                                        <div>
+                                            <label class="product-name"><span>Prodotto:</span>{{$product->collection->brand->name}} {{$product->collection->name}} - {{$product->cod}}</label>
+                                        </div>
+                                        <div>
+                                            <label><span>Prezzo:</span>{{$product->pivot->price}} €</label>
+                                        </div>
+                                        <div>
+                                            <label><span>Quantità Acquistata:</span>{{$product->pivot->quantity}}</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            @endforeach
+
+                        </div>
+
                     </div>
                 @endforeach
             </div>
