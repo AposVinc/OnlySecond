a74389452043333f490e7651c3514fcefa82fb03
AposVinc <apos.vincenzo@gmail.com>
2019-10-21 16:04:18 +0200

    .

--- resources/views/frontend/profile/review.blade.php
+++ resources/views/frontend/profile/review.blade.php
@@ -9,43 +9,6 @@
                 Le Mie Recensioni
             @endcomponent
 
-            <div class="col-lg-12 mt_20">
-                <div class="category-page-wrapper mb_30">
-                    <div class="list-grid-wrapper pull-right">
-                        <div class="btn-group btn-list-grid">
-                            <button type="button" id="grid-view" class="btn btn-default grid-view active"></button>
-                            <button type="button" id="list-view" class="btn btn-default list-view"></button>
-                        </div>
-                    </div>
-                    <div class="page-wrapper pull-right">
-                        <label class="control-label" for="input-limit">Mostra :</label>
-                        <div class="limit">
-                            <select id="input-limit" class="form-control">
-                                <option value="ASC" selected="selected">Tutte</option>
-                                <option value="ASC">Recenti</option>
-                                <option value="DESC">Meno Recenti</option>
-                            </select>
-                        </div>
-                        <span><i class="fa fa-angle-down" aria-hidden="true"></i></span>
-                    </div>
-                    <div class="sort-wrapper pull-right">
-                        <label class="control-label" for="input-sort">Brand :</label>
-                        <div class="sort-inner">
-                            <select id="input-sort" class="form-control">
-                                <option value="ASC" selected="selected">Tutti</option>
-                                <option value="ASC">Breil</option>
-                                <option value="DESC">Tissot</option>
-                                <option value="ASC">Wellington</option>
-                                <option value="DESC">Fossil</option>
-                                <option value="DESC">Lacoste</option>
-                                <option value="ASC">Ice Watch</option>
-                            </select>
-                        </div>
-                        <span><i class="fa fa-angle-down" aria-hidden="true"></i></span>
-                    </div>
-                </div>
-            </div>
-
             @foreach(auth()->user()->reviews as $review)
                 <div class="col-lg-12 mb_40">
                     <div class="mt_10 col-md-2">
--- resources/views/frontend/profile/review2.blade.php
+++ resources/views/frontend/profile/review2.blade.php
@@ -1,4 +1,3 @@
-@@ -0,0 +1,78 @@
 @extends('frontend.layout')
 
 @section('content')
