b65e0159e38449c42d5a0b46e2efc6942dff949c
AposVinc <apos.vincenzo@gmail.com>
2019-10-24 18:19:52 +0200

    wishlist

--- resources/views/frontend/profile/wishlist.blade.php
+++ resources/views/frontend/profile/wishlist.blade.php
@@ -29,29 +29,40 @@
 
                             </div>
                             <div id="wishlist" class="col-md-10 mt_10">
-                                <div>
-                                    <div style="position: absolute; top: 0; right: 10px;">
+                                    <div class="button-right">
                                         <a href="" type="button" class="btn btn-outline fa fa-shopping-cart" ></a>
                                         <a href="{{route('Wishlist.RemoveProduct',['cod'=>$product->cod])}}" type="button" class="btn btn-outline fa fa-trash"></a>
                                     </div>
-                                </div>
                                 <div class="mt_10">
-                                    <label class="product-price"><span>Prezzo:</span><del>{{$product->price}}€</del>{{$product->offer->calculateDiscount()}} €</label>
-                                    <label class="product-attr"><span>Genere:</span>{{$product->genre}}</label>
-                                    <label class="product-attr"><span>Categoria:</span>
-                                        @php
-                                            $stringa = "";
-                                            foreach($product->categories as $category){
-                                                if($stringa==""){
-                                                    $stringa = $stringa. $category->name;
-                                                }else{
-                                                    $stringa = $stringa. ", ". $category->name;
+                                    <div class="product-price">
+                                        <span>Prezzo:</span>
+                                        <label>
+                                            <del>{{$product->price}}€</del>
+                                            {{$product->offer->calculateDiscount()}} €
+                                        </label>
+                                    </div>
+                                    <div class="product-attr">
+                                        <span>Genere:</span><label>{{$product->genre}}</label>
+                                    </div>
+                                    <div class="product-attr">
+                                        <span>Categoria:</span>
+                                        <label>
+                                            @php
+                                                $stringa = "";
+                                                foreach($product->categories as $category){
+                                                    if($stringa==""){
+                                                        $stringa = $stringa. $category->name;
+                                                    }else{
+                                                        $stringa = $stringa. ", ". $category->name;
+                                                    }
                                                 }
-                                            }
-                                            echo $stringa;
-                                        @endphp
-                                    </label>
-                                    <p class="product-desc">{{Str::limit($product->long_desc,410)}}</p>
+                                                echo $stringa;
+                                            @endphp
+                                        </label>
+                                    </div>
+                                    <div class="product-desc">
+                                        <p>{{Str::limit($product->long_desc,410)}}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -69,29 +80,38 @@
                                 </div>
                             </div>
                             <div id="wishlist" class="col-md-10 mt_10">
-                                <div>
-                                    <div style="position: absolute; top: 0; right: 10px;">
+                                    <div class="button-right">
                                         <a href="" type="button" class="btn btn-outline fa fa-shopping-cart" ></a>
                                         <a href="{{route('Wishlist.RemoveProduct',['cod'=>$product->cod])}}" type="button" class="btn btn-outline fa fa-trash"></a>
                                     </div>
-                                </div>
                                 <div>
-                                    <label class="product-price"><span>Prezzo:</span>{{$product->price}} €</label>
-                                    <label class="product-attr"><span>Genere:</span>{{$product->genre}}</label>
-                                    <label class="product-attr"><span>Categoria:</span>
-                                        @php
-                                            $stringa = "";
-                                            foreach($product->categories as $category){
-                                                if($stringa==""){
-                                                    $stringa = $stringa. $category->name;
-                                                }else{
-                                                    $stringa = $stringa. ", ". $category->name;
+                                    <div class="product-price">
+                                        <span>Prezzo:</span><label>{{$product->price}}€</label>
+                                    </div>
+                                    <div class="product-attr">
+                                        <span>Genere:</span>
+                                        <label>{{$product->genre}}</label>
+                                    </div>
+                                    <div class="product-attr">
+                                        <span>Categoria:</span>
+                                        <label>
+                                            @php
+                                                $stringa = "";
+                                                foreach($product->categories as $category){
+                                                    if($stringa==""){
+                                                        $stringa = $stringa. $category->name;
+                                                    }else{
+                                                        $stringa = $stringa. ", ". $category->name;
+                                                    }
                                                 }
-                                            }
-                                            echo $stringa;
-                                        @endphp
-                                    </label>
-                                    <p class="product-desc"><span>Descrizione:</span>{{Str::limit($product->long_desc,550)}}</p>
+                                                echo $stringa;
+                                            @endphp
+                                        </label>
+                                    </div>
+                                    <div class="product-desc">
+                                        <span>Descrizione:</span>
+                                        <p>{{Str::limit($product->long_desc,550)}}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
