6941d214504660e1d10692db16cf780203e39f1f
AposVinc <apos.vincenzo@gmail.com>
2019-10-24 12:48:55 +0200

    aggiustamento html chronologi e review

--- resources/views/frontend/profile/chronology.blade.php
+++ resources/views/frontend/profile/chronology.blade.php
@@ -15,20 +15,20 @@
 
                     <div class="heading-part mb_10">
                         <div class="col-md-12">
-                            <div class="pl_0 col-md-2 ">
-                                <label class="label-order-info">N° Ordine: </label>
-                                <label class="order-info">{{$order->id}}</label>
+                            <div class="order-info pl_0 col-md-2 ">
+                                <span>N° Ordine:</span>
+                                <label>{{$order->id}}</label>
                             </div>
-                            <div class="pl_0 col-md-3">
-                                <div class="order-info" style="color: #fff;" >Data: </div>
-                                <div class="order-info">{{date('d-m-Y H:i', strtotime($order->created_at))}}</div>
+                            <div class="order-info pl_0 col-md-3">
+                                <label class="mr_5" style="color: #fff;">Data: </label>
+                                <label>{{date('d-m-Y H:i', strtotime($order->created_at))}}</label>
                             </div>
-                            <div class="col-md-offset-2 col-md-2">
-                                <label class="order-info" style="color: #fff;">Regalo: </label>
-                                <label class="order-info">@if($order->gift) Si @else No @endif</label>
+                            <div class="col-md-offset-2 col-md-2 order-info">
+                                <label class="mr_5" style="color: #fff;">Regalo:</label>
+                                <label>@if($order->gift) Si @else No @endif</label>
                             </div>
-                            <div class="pull-right">
-                                <label class="label-order-info">Prezzo Totale: </label>
+                            <div class="pull-right order-info">
+                                <span>Prezzo Totale:</span>
                                 <label class="order-price">{{$order->calculateTotalPrice()}} €</label>
                             </div>
                         </div>
@@ -37,17 +37,21 @@
                         <div class="col-lg-12 mb_20">
                             <div id="order-attr" class="mt_10 mr_5 col-md-3 pull-right">
                                 <div>
-                                    <label><span>Indirizzo di Fatturazione:</span><br>{{$order->billingAddress->name}} {{$order->billingAddress->surname}} <br> {{$order->billingAddress->address}}, {{$order->billingAddress->civic_number}}<br>{{$order->billingAddress->city}} {{'('. $order->billingAddress->region. ')'}} CAP:{{$order->billingAddress->zip}}</label>
+                                    <span>Indirizzo di Fatturazione:</span>
+                                    <label>{{$order->billingAddress->name}} {{$order->billingAddress->surname}} <br> {{$order->billingAddress->address}}, {{$order->billingAddress->civic_number}}<br>{{$order->billingAddress->city}} {{'('. $order->billingAddress->region. ')'}} CAP:{{$order->billingAddress->zip}}</label>
                                 </div>
                                 <div>
-                                    <label><span>Metodo di Pagamento:</span><br>{{$order->payment->name}}</label>
+                                    <span>Metodo di Pagamento:</span>
+                                    <label>{{$order->payment->name}}</label>
                                 </div>
 
                                 <div>
-                                    <label><span>Indirizzo di Spedizione:</span><br>{{$order->mailingAddress->name}} {{$order->mailingAddress->surname}} <br> {{$order->mailingAddress->address}}, {{$order->mailingAddress->civic_number}}<br>{{$order->mailingAddress->city}} {{'('. $order->mailingAddress->region. ')'}} CAP:{{$order->mailingAddress->zip}}</label>
+                                    <span>Indirizzo di Spedizione:</span>
+                                    <label>{{$order->mailingAddress->name}} {{$order->mailingAddress->surname}} <br> {{$order->mailingAddress->address}}, {{$order->mailingAddress->civic_number}}<br>{{$order->mailingAddress->city}} {{'('. $order->mailingAddress->region. ')'}} CAP:{{$order->mailingAddress->zip}}</label>
                                 </div>
                                 <div>
-                                    <label><span>Corriere:</span>{{$order->courier->name}}</label>
+                                    <span>Corriere:</span>
+                                    <label>{{$order->courier->name}}</label>
                                 </div>
                             </div>
                             @foreach($order->products as $product)
@@ -60,21 +64,23 @@
                                         </div>
                                     </div>
                                     <div class="mt_10 pl_0 col-md-8">
-                                        <div>
-                                            <label class="product-name"><span>Prodotto:</span>{{$product->collection->brand->name}} {{$product->collection->name}} - {{$product->cod}}</label>
+                                        <div class="product-name">
+                                            <span>Prodotto:</span>
+                                            <label>{{$product->collection->brand->name}} {{$product->collection->name}} - {{$product->cod}}</label>
                                         </div>
-                                        <div>
-                                            <label class="product-price"><span>Prezzo:</span>{{$product->pivot->price}} €</label>
+                                        <div class="product-price">
+                                            <span>Prezzo:</span>
+                                            <label>{{$product->pivot->price}} €</label>
                                         </div>
-                                        <div>
-                                            <label class="product-attr"><span>Quantità Acquistata:</span>{{$product->pivot->quantity}}</label>
+                                        <div class="product-attr">
+                                            <span>Quantità Acquistata:</span>
+                                            <label>{{$product->pivot->quantity}}</label>
                                         </div>
                                     </div>
                                 </div>
                             @endforeach
 
                         </div>
-
                     </div>
                 @endforeach
             </div>
--- resources/views/frontend/profile/review.blade.php
+++ resources/views/frontend/profile/review.blade.php
@@ -12,8 +12,8 @@
             @foreach(auth()->user()->reviews as $review)
                 <div class="col-lg-12 mb_40">
                     <div class="heading-part mt_0 mb_10">
-                        <label class="sub_title"><span>Prodotto:</span></label>
-                        <div class="product-info">{{$review->product->collection->brand->name}} {{$review->product->collection->name}} - {{$review->product->cod}}</div>
+                        <span class="sub_title">Prodotto:</span>
+                        <label class="product-info">{{$review->product->collection->brand->name}} {{$review->product->collection->name}} - {{$review->product->cod}}</label>
                     </div>
 
                     <div class="mt_10 col-md-2">
@@ -24,26 +24,27 @@
                         </div>
                     </div>
                     <div id="review" class="col-md-10 mt_10">
-                        <div style="position: absolute; top: 0; right: 10px;">
+                        <div class="button-right">
                             <a href="{{route('Product', ['cod' => $review->product->cod])}}" type="button" class="btn btn-outline fa fa-pencil" title="Modifica Recensione"></a>
                             <a href="{{route('Review.Remove', ['id' => $review->id])}}" type="button" class="btn btn-outline fa fa-trash"></a>
                         </div>
-                        <div >
-                            <label class="review-title"><span style="margin-right: 8px">Titolo:</span>{{$review->title}}</label>
-                            <label class="review-vote-date">
-                                <span>Data:</span>{{date('d-m-Y H:i', strtotime($review->created_at))}}
-                                <span style="margin-left: 25px;">Voto:</span>
-                                @for ($i = 1; $i < 6; $i++)
-                                    @if($i <= $review->vote)
-                                        <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i><i class="fa fa-star fa-stack-1x"></i></span>
-                                    @else
-                                        <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
-                                    @endif
-                                @endfor
-                            </label>
+                        <div class="review-title">
+                            <span>Titolo:</span>
+                            <label>{{$review->title}}</label>
+                        </div>
+                        <div class="review-vote-date">
+                            <span class="mr_5">Data:</span><label>{{date('d-m-Y H:i', strtotime($review->created_at))}}</label>
+                            <span style="margin-left: 25px;">Voto:</span>
+                            @for ($i = 1; $i < 6; $i++)
+                                @if($i <= $review->vote)
+                                    <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i><i class="fa fa-star fa-stack-1x"></i></span>
+                                @else
+                                    <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
+                                @endif
+                            @endfor
                         </div>
                         <div class="review-text">
-                            <span>{{$review->text}}</span>
+                            <p>{{$review->text}}</p>
                         </div>
                     </div>
                 </div>
