3b1fe5c02dba16b19714566540477167ae59c0cf
AposVinc <apos.vincenzo@gmail.com>
2019-10-26 13:31:13 +0200

    EditProfile

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -386,19 +386,19 @@
             if($('#change_psw').is(':checked')){
                 if($('#password').children().length != 4){
                     var old_password = $(' <div class="form-group required">\n' +
-                        '                                    <label for="old-password" id="label-old-password" class="col-sm-3 control-label">Vecchia Password</label>\n' +
+                        '                                    <label for="old-password" id="label-old-password" class="col-sm-3 control-label" style="padding-top: 0; top: -5px">Vecchia Password</label>\n' +
                         '                                    <div class="col-sm-9">\n' +
                         '                                        <input type="password" class="form-control" id="old-password" placeholder="Vecchia Password" value="" name="old-password">\n' +
                         '                                    </div>\n' +
                         '                                </div>');
                     var new_password = $('<div class="form-group required">\n' +
-                        '                                    <label for="new-password" id="label-new-password" class="col-sm-3 control-label">Nuova Password</label>\n' +
+                        '                                    <label for="new-password" id="label-new-password" class="col-sm-3 control-label" style="padding-top: 0; top: -5px">Nuova Password</label>\n' +
                         '                                    <div class="col-sm-9">\n' +
                         '                                        <input type="password" class="form-control" id="new-password" placeholder="Nuova Password" value="" name="new-password">\n' +
                         '                                    </div>\n' +
                         '                                </div>'    );
                     var confirm_new_password = $('<div class="form-group required">\n' +
-                        '                                    <label for="confirm-new-password" id="label-confirm-new-password" class="col-sm-3 control-label">Conferma Nuova Password</label>\n' +
+                        '                                    <label for="confirm-new-password" id="label-confirm-new-password" class="col-sm-3 control-label" style="padding-top: 0; top: -5px">Conferma Nuova Password</label>\n' +
                         '                                    <div class="col-sm-9">\n' +
                         '                                        <input type="password" class="form-control" id="confirm-new-password" placeholder="Conferma Nuova Password" value="" name="confirm-new-password">\n' +
                         '                                    </div>\n' +
