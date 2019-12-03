492ae5b67514a87f9fd0a8a11d0a3a8a26557580
AposVinc <apos.vincenzo@gmail.com>
2019-12-03 18:29:36 +0100

    Update message.blade.php

--- resources/views/frontend/partials/message.blade.php
+++ resources/views/frontend/partials/message.blade.php
@@ -1,8 +1,14 @@
+@if(session('success') or session('error'))
     <div class="col-sm-12">
+        @if(session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
             </div>
+        @endif
+        @if(session('error'))
             <div class="alert alert-danger">
                 {{ session('error') }}
             </div>
+        @endif
     </div>
+@endif
